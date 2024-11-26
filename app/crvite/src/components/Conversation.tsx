63dc8e0ea06926a834e24227d748a560dad7384b
arunwidjaja <arun.widjaja@gmail.com>
2024-11-26 15:13:18 -0800

    update gitignore

--- app/crvite/src/components/Conversation.tsx
+++ app/crvite/src/components/Conversation.tsx
@@ -22,8 +22,7 @@ export function Conversation() {
     return (
         <div
             id="conversation"
-            className="border border-red-500 flex flex-1 flex-col-reverse text-text bg-none output p-2 overflow-y-auto [&::-webkit-scrollbar]:w-1.5 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:bg-accent [&::-webkit-scrollbar-thumb]:rounded-full">
-
+            className="flex flex-1 flex-col-reverse text-text bg-none output p-2 overflow-y-auto [&::-webkit-scrollbar]:w-1.5 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:bg-accent [&::-webkit-scrollbar-thumb]:rounded-full">
             {messages.map((msg: Message, index: number) => (<ChatBubble key={index} message={msg} />))}
         </div>
     )
